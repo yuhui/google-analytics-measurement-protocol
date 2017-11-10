5f5feab565a00c2116ed09c6517aa7d7500089ca
Yuhui <yuhuibc@gmail.com>
2017-11-10 17:24:04 +0800

    Revert "get name, author, version and license from the package directly"
    This reverts commit 79ed914735841117fa7c0f6bedd264e297108b7b.

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
-from google.analytics.measurement_protocol import __name__ as gamp_name, __author__ as gamp_author, __license__ as gamp_license, __version__ as gamp_version
 
 setup(
-    name=gamp_name,
-    version=gamp_version,
+    name='google-analytics-measurement-protocol',
+    version='0.1dev1',
     description='Send hits to Google Analytics through its Measurement Protocol API.',
     long_description=open('README.md').read(),
-    author=gamp_author,
+    author='Yu Hui',
     author_email='yuhuibc@gmail.com',
     packages=find_packages(),
     install_requires=['requests>=2.0,<3.0a0'],
-    license=gamp_license,
+    license='License :: OSI Approved :: MIT License',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 1 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
